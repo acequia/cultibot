0a4c9cf98d4bba49c374794c7da5241c2176d2a3
Matías Battocchia <matias@riseup.net>
2017-04-11 01:42:48 -0300

    Tests passing for sensor-controlled outputs

---
+++ sensor_control.lua
@@ -0,0 +1,30 @@
+function sensor_condition(module)
+  local measure = sensors[module.var]()
+  local high    = module.set + module.hist / 2
+  local low     = module.set - module.hist / 2
+
+  -- Sensor control features a simple hysteresis cycle for two use cases:
+  --
+  -- The first one for ventilation, where an actuator must be activated
+  -- when the temperature rises above the high threshold level and deactivated
+  -- when the temperature falls below the low threshold level.
+  --
+  -- The second one is the inverse one, for heating. The actuator must go off
+  -- above the high threshold and go on below the low threshold.
+
+  if module.effect == 'decrease' then
+    if module.status then -- on
+      if measure < low  then module.status = false end
+    else -- off
+      if measure > high then module.status = true  end
+    end
+  else -- effect == 'increase'
+    if module.status then -- on
+      if measure > high then module.status = false end
+    else -- off
+      if measure < low  then module.status = true  end
+    end
+  end
+
+  return module.status
+end
---
+++ test_sensor_control.lua
@@ -0,0 +1,60 @@
+dofile('sensor_control.lua')
+
+sensors = {}
+sensors.temperature = function() return t end
+
+-- Ventilation (case 1)
+
+ventilation = {
+  var    = 'temperature',
+  set    = 24,
+  hist   =  2, -- histeresis
+  effect = 'decrease',
+}
+
+t = 24
+assert(not sensor_condition(ventilation) )
+t = 25
+assert(not sensor_condition(ventilation) )
+t = 26
+assert(    sensor_condition(ventilation) )
+t = 25
+assert(    sensor_condition(ventilation) )
+t = 24
+assert(    sensor_condition(ventilation) )
+t = 23
+assert(    sensor_condition(ventilation) )
+t = 22
+assert(not sensor_condition(ventilation) )
+t = 23
+assert(not sensor_condition(ventilation) )
+t = 24
+assert(not sensor_condition(ventilation) )
+
+-- Heating (case 2)
+
+heating = {
+  var    = 'temperature',
+  set    = 24,
+  hist   =  2, -- histeresis
+  effect = 'increase',
+}
+
+t = 24
+assert(not sensor_condition(heating) )
+t = 25
+assert(not sensor_condition(heating) )
+t = 26
+assert(not sensor_condition(heating) )
+t = 25
+assert(not sensor_condition(heating) )
+t = 24
+assert(not sensor_condition(heating) )
+t = 23
+assert(not sensor_condition(heating) )
+t = 22
+assert(    sensor_condition(heating) )
+t = 23
+assert(    sensor_condition(heating) )
+t = 24
+assert(    sensor_condition(heating) )
