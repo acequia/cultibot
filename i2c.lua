07d463f87cc3abdc0096e73a2cc718172659dfd0
Matías Battocchia <matias@riseup.net>
2017-05-10 00:57:22 -0300

    Improved I2C code with bit operations

--- i2c.lua
+++ i2c.lua
@@ -8,10 +8,9 @@ pcf8574 = 0x20 -- I/O expander
 
 i2c.setup(id, sda, scl, i2c.SLOW)
 
-io      = {}
-io.mask = {}
-io.map  = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}
-io.acu  = 0xFF
+io       = {}
+io.map   = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}
+io.state = 0xFF -- off
 
 io.write = function(value)
   i2c.start(id)
@@ -28,25 +27,12 @@ io.read = function()
   return data
 end
 
-io.send = function()
-  local acu = 0xFF
-
-  for i, o in pairs(io.mask) do
-    if o then acu = acu - io.map[i] end
-  end
-
-  if io.acu ~= acu then
-    io.acu = acu
-    io.write(acu)
-  end
-end
-
 io.on = function(output)
-  io.mask[output] = true
-  io.send()
+  io.state = bit.clear(io.state, output)
+  io.write(io.state)
 end
 
 io.off = function(output)
-  io.mask[output] = false
-  io.send()
+  io.state = bit.set(io.state, output)
+  io.write(io.state)
 end
--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 uart.setup(0, 115200, 8, 0, 1, 1)
 
 dofile('wifi_event_monitor.lua')
---dofile('settings.lua')
---dofile('i2c.lua')
---dofile('am2320.lua')
---dofile('time_control.lua')
---dofile('sensor_control.lua')
---dofile('relay_logic.lua')
+dofile('settings.lua')
+dofile('i2c.lua')
+dofile('am2320.lua')
+dofile('time_control.lua')
+dofile('sensor_control.lua')
+dofile('relay_logic.lua')
 
 -- Enable network configuration within time window
 --dofile('config.lua')
