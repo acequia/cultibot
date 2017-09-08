41b7c289f69709f6e71a5ac20bdde70f4ddc070e
Matías Battocchia <matias@riseup.net>
2017-09-08 10:10:23 -0300

    minor modifications in order to test SNTP

--- wifi_event_monitor.lua
+++ wifi_event_monitor.lua
@@ -1,5 +1,12 @@
 wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-  sntp.sync('pool.ntp.org', nil, nil, 'autorepeat')
+  sntp.sync('pool.ntp.org',
+            function(sec, usec, server, info)
+              print('Synced', sec, usec, server)
+            end,
+            function()
+              print('Sync failed!')
+            end,
+            'autorepeat')
 
   m:connect('conejo.mauriciopasquier.com.ar')
 end)
